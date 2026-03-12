d945524d51eb28a908345d7cee626e5a77847a47
Qu5antum <erae88959@gmail.com>
2026-03-12 09:27:20 +0300

    user model and seller profile

---
+++ config/user_service/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=30, blank=True)
+    address = models.TextField(blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class SellerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="seller_profile")
+    store_name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    logo = models.ImageField(upload_to='store_logos', blank=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.email}"
+
+
