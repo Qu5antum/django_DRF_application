e82310cc2990e4e5ae723d7eafa73bdc58e04adb
Qu5antum <erae88959@gmail.com>
2026-03-12 11:00:55 +0300

    serializers for user

---
+++ config/user_service/serializers.py
@@ -0,0 +1,79 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import User, SellerProfile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name']
+
+
+class SellerProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SellerProfile
+        fields = ['address', 'store_name', 'description', 'created_at']
+
+
+class BecomeSellerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = SellerProfile
+        fields = ["store_name", "description"]
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'password', 'password_confirm']
+
+        def validate(self, attrs):
+            if attrs['password'] != attrs['password_confirm']:
+                raise serializers.ValidationError("Password do not match")
+            return attrs
+        
+        def create(self, validated_data):
+            validated_data.pop('password_confirm')
+            user = User.objects.create_user(**validated_data)
+            return user
+        
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if not email or not password:
+            raise serializers.ValidationError(
+                "Email and password are required."
+            )
+
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password
+        )
+
+        if not user:
+            raise serializers.ValidationError(
+                "Invalid email or password."
+            )
+        
+        refresh = RefreshToken.for_user(user)
+
+        attrs["user"] = user
+        attrs["access"] = str(refresh.access_token)
+        attrs["refresh"] = str(refresh)
+
+        return attrs
+
+
+    
